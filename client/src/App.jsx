78b047077a523555e6e63170b137b8e09ecda453
zohar <zoharsim2000@gmail.com>
2026-05-04 18:51:18 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+//import "./App.css";
+import MyButton from "./components/MyButton/MyButton";
+
+export default function App() {
+  return (
+    <div>
+      <h2> welcome to eToro dashboard!</h2>
+      <MyButton />
+    </div>
+  );
+}
---
+++ client/src/components/MyButton/MyButton.jsx
@@ -0,0 +1,9 @@
+import "./MyButton.css";
+
+export default function MyButton() {
+  return (
+    <div className="container">
+      <button className="button">press me!</button>
+    </div>
+  );
+}
