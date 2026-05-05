b8ce49a52b53f39010bfa48c31e28152189943c3
zohar <zoharsim2000@gmail.com>
2026-05-05 10:18:31 +0300

    connecting to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,34 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { MongoClient } from "mongodb";
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+const uri = process.env.MONGO_URI;
+if (!uri) {
+  throw new Error("MONGODB_URI is not set in .env");
+}
+const client = new MongoClient(uri);
+
+let usageCollection;
+
+async function connectDB() {
+  await client.connect();
+  const db = client.db("AIDashboard");
+  usageCollection = db.collection("usage");
+  console.log("connected to mongo");
+}
+
+try {
+  connectDB();
+} catch (e) {
+  console.log(e);
+}
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log("app is listening on port" + PORT);
